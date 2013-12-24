ca0b5018725433d060bee8c00bdfec0c418a3ad3
Erin Spiceland <erin@thespicelands.com>
2013-12-24 12:27:40 -0600

    Add simple file server.

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
-var express = require("express");
-var logfmt = require("logfmt");
-var app = express();
-
-app.use(logfmt.requestLogger());
-
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
-
+var static = require('node-static');
+var fileServer = new static.Server('./public');
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        fileServer.serve(request, response, function (err, result) {
+            if (err) {
+                switch (err.status) {
+                case 500:
+                case 404:
+                    fileServer.serveFile('/' + err.status + '.html', err.status, {}, request, response);
+                    break;
+                default:
+                    // Respond to the client
+                    response.writeHead(err.status, err.headers);
+                    response.end();
+                }
+            }
+        });
+    }).resume();
+}).listen(port);
+
