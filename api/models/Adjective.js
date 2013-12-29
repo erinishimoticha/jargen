3e18e746303b69a6bd72205e8da29a0b1c8e3038
Erin Spiceland <erin@thespicelands.com>
2013-12-29 14:16:59 -0600

    Switch to Sails, Bootstrap.

---
+++ api/models/Adjective.js
@@ -0,0 +1,14 @@
+/**
+ * Adjective
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+  schema: true,
+  attributes: {
+    word: {'type': 'string', 'required': true} 
+  }
+};
